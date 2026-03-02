12d9ff17e8b5a6538f5388b3a0563e263108adde
Jamie <jamie7williams@gmail.com>
2026-03-02 18:19:52 +0000

    chore: update style.css dep array and enqueue main.css in enqueue.php

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -8,6 +8,20 @@ add_action( 'wp_enqueue_scripts', 'ecs_enqueue_scripts' );
 
 
 function ecs_enqueue_scripts() {
-	$ecs_version = '1.0.0';
-	wp_enqueue_style( 'ecs-styles-css', get_stylesheet_uri(), $ecs_version );
+	$template_uri = get_template_directory_uri();
+	$version      = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style(
+		'ecs-styles-css',
+		get_stylesheet_uri(),
+		array(),
+		$version
+	);
+
+	wp_enqueue_style(
+		'ecs-main-css',
+		$template_uri . '/assets/css/main.css',
+		array(),
+		$version
+	);
 }
