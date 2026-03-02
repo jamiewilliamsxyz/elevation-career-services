e7c6eecafcf85647f23acc29ca687aa3c169e1bd
Jamie <jamie7williams@gmail.com>
2026-03-02 15:01:54 +0000

    chore(theme setup): load text domain in theme-setup.php

--- includes/theme-setup.php
+++ includes/theme-setup.php
@@ -6,6 +6,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action( 'after_setup_theme', 'ecs_setup_theme' );
 
 function ecs_setup_theme() {
+	// Load Text Domain
+	load_theme_textdomain(
+		'elevation-career-services',
+		get_template_directory() . '/languages'
+	);
+
+	// Theme Support
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'editor-styles' );
