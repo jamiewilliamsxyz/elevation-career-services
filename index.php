448bc84d51ca02197322ea89f7b8dae67ed700ac
Jamie <jamie7williams@gmail.com>
2026-03-05 13:22:33 +0000

    chore: add index.php fallback template

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php get_header(); ?>
-<main></main>
+
+<main>
+	<section class="ecs-section-md">
+		<div class="ecs-container">
+			<?php
+			if ( have_posts() ) :
+				?>
+				<div class="ecs-post-cards-container">
+					<?php
+					while ( have_posts() ) :
+						the_post();
+						get_template_part( 'template-parts/content/content' );
+					endwhile;
+					?>
+				</div>
+				<?php
+			else :
+				get_template_part( 'template-parts/components/no-posts' );
+			endif;
+			?>
+		</div>
+	</section>
+</main>
+
 <?php get_footer(); ?>
