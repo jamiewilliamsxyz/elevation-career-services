e726447532d797b8565be723016d8e65de52b116
Jamie <jamie7williams@gmail.com>
2026-03-07 11:16:16 +0000

    chore: add section template part calls to front-page.php

--- front-page.php
+++ front-page.php
@@ -2,12 +2,18 @@
 
 <main>
 	<?php
+	get_template_part( 'template-parts/sections/hero' );
+	get_template_part( 'template-parts/sections/stats-bar' );
+
 	if ( have_posts() ) :
 		while ( have_posts() ) :
 			the_post();
 			the_content();
 		endwhile;
 	endif;
+
+	get_template_part( 'template-parts/sections/about-snippet' );
+	get_template_part( 'template-parts/sections/cta-banner' );
 	?>
 </main>
 
