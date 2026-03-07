e6926e201db1b931568d236b2d5afddc371edc45
Jamie <jamie7williams@gmail.com>
2026-03-07 20:04:55 +0000

    feat: add cta banner with styles

--- template-parts/sections/cta-banner.php
+++ template-parts/sections/cta-banner.php
@@ -1,5 +1,7 @@
-<section class="ecs-section-md">
+<section class="ecs-section-md ecs-cta-banner">
 	<div class="ecs-container">
-		
+		<h2><?php esc_html_e( 'Ready To Land Your Next Role?', 'elevation-career-services' ); ?></h2>
+		<p><?php esc_html_e( "Join hundreds of professionals who've landed their dream role", 'elevation-career-services' ); ?></p>
+		<button class="ecs-btn"><?php esc_html_e( 'Get Started', 'elevation-career-services' ); ?></button>
 	</div>
 </section>
