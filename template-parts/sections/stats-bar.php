29f83b1d9cf6c62a8faad1804dfb8aa047ac72ef
Jamie <jamie7williams@gmail.com>
2026-03-07 19:50:22 +0000

    chore: escaped and made text translatable in stats-bar.php

--- template-parts/sections/stats-bar.php
+++ template-parts/sections/stats-bar.php
@@ -2,17 +2,17 @@
 	<div class="ecs-container">
 		<div class="ecs-stat">
 			<p class="ecs-stat-number">50+</p>
-			<p class="ecs-stat-text">Professionals Helped</p>
+			<p class="ecs-stat-text"><?php esc_html_e( 'Professionals Helped', 'elevation-career-services' ); ?></p>
 		</div>
 
 		<div class="ecs-stat">
 			<p class="ecs-stat-number">200+</p>
-			<p class="ecs-stat-text">CVs Written</p>
+			<p class="ecs-stat-text"><?php esc_html_e( 'CVs Written', 'elevation-career-services' ); ?></p>
 		</div>
 
 		<div class="ecs-stat">
 			<p class="ecs-stat-number">98%</p>
-			<p class="ecs-stat-text">Client Satisfaction</p>
+			<p class="ecs-stat-text"><?php esc_html_e( 'Client Satisfaction', 'elevation-career-services' ); ?></p>
 		</div>
 	</div>
 </section>
