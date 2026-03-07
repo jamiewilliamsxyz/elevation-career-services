54bd31d2bcf5226142977e0b1276f1fda7d0e3fd
Jamie <jamie7williams@gmail.com>
2026-03-07 20:22:12 +0000

    feat: add about snippet with styles

--- template-parts/sections/about-snippet.php
+++ template-parts/sections/about-snippet.php
@@ -1,5 +1,13 @@
-<section class="ecs-section-lg">
+<section class="ecs-section-lg ecs-about-snippet">
 	<div class="ecs-container">
+		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/about-img.jpg" alt="Group of work colleagues sitting around a wooden table, having a business meeting" />
 		
+		<div>
+			<h2><?php esc_html_e( 'About Elevation Career Services', 'elevation-career-services' ); ?></h2>
+			<p><?php esc_html_e( 'We, at Elevation Career Services, believe that all individuals deserve to have a fulfilling career. We are here to help ensure that your application is distinguished from the rest of the competition, whether you are looking to climb the corporate ladder, transition to a new industry, or get back to work.', 'elevation-career-services' ); ?></p>
+			<p><?php esc_html_e( 'All of our services, including our resume writing and one-on-one Zoom video interview preparation, are tailored to your specific needs, industry, and story. We offer honest, specialized, and effective career assistance, not templates and generic advice.', 'elevation-career-services' ); ?></p>
+			<p><?php esc_html_e( 'Dozens of professionals across the UK have taken advantage of our assistance to get jobs that they are proud to go to each day. With our help, you get a team that is invested in your success, not just a better resume.', 'elevation-career-services' ); ?></p>
+			<a><?php esc_html_e( 'Learn More About Us', 'elevation-career-services' ); ?></a>
+		</div>
 	</div>
 </section>
