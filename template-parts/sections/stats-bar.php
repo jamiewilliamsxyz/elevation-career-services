4a97ba0b7e3ffd850a1afe465e67bcf045e07cd7
Jamie <jamie7williams@gmail.com>
2026-03-07 19:46:56 +0000

    feat: add stats bar with styles

--- template-parts/sections/stats-bar.php
+++ template-parts/sections/stats-bar.php
@@ -1,5 +1,18 @@
-<section class="ecs-section-sm">
+<section class="ecs-section-sm ecs-stats-bar">
 	<div class="ecs-container">
-		
+		<div class="ecs-stat">
+			<p class="ecs-stat-number">50+</p>
+			<p class="ecs-stat-text">Professionals Helped</p>
+		</div>
+
+		<div class="ecs-stat">
+			<p class="ecs-stat-number">200+</p>
+			<p class="ecs-stat-text">CVs Written</p>
+		</div>
+
+		<div class="ecs-stat">
+			<p class="ecs-stat-number">98%</p>
+			<p class="ecs-stat-text">Client Satisfaction</p>
+		</div>
 	</div>
 </section>
