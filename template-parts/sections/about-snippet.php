637c9b0d83ade7858a2bd1b4ec43cde4bb79b3e1
Jamie <jamie7williams@gmail.com>
2026-03-08 20:17:38 +0000

    feat: add Customizer integration to about snippet section

--- template-parts/sections/about-snippet.php
+++ template-parts/sections/about-snippet.php
@@ -1,13 +1,34 @@
+<?php
+$img       = get_theme_mod( 'ecs_about_snippet_img' );
+$heading   = get_theme_mod( 'ecs_about_snippet_heading' );
+$content   = get_theme_mod( 'ecs_about_snippet_content' );
+$link_text = get_theme_mod( 'ecs_about_snippet_link_text' );
+$link      = get_theme_mod( 'ecs_about_snippet_link' );
+
+// Don't render the section if there's no meaningful content
+if ( ! $content ) {
+	return;
+}
+?>
+
 <section class="ecs-section-lg ecs-about-snippet">
 	<div class="ecs-container">
-		<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/about-img.jpg" alt="Group of work colleagues sitting around a wooden table, having a business meeting" />
-		
+		<?php if ( $img ) : ?>
+			<img src="<?php echo esc_url( $img ); ?>" alt="<?php esc_attr_e( 'About image', 'elevation-career-services' ); ?>" />
+		<?php endif ?>
+
 		<div>
-			<h2><?php esc_html_e( 'About Elevation Career Services', 'elevation-career-services' ); ?></h2>
-			<p><?php esc_html_e( 'We, at Elevation Career Services, believe that all individuals deserve to have a fulfilling career. We are here to help ensure that your application is distinguished from the rest of the competition, whether you are looking to climb the corporate ladder, transition to a new industry, or get back to work.', 'elevation-career-services' ); ?></p>
-			<p><?php esc_html_e( 'All of our services, including our resume writing and one-on-one Zoom video interview preparation, are tailored to your specific needs, industry, and story. We offer honest, specialized, and effective career assistance, not templates and generic advice.', 'elevation-career-services' ); ?></p>
-			<p><?php esc_html_e( 'Dozens of professionals across the UK have taken advantage of our assistance to get jobs that they are proud to go to each day. With our help, you get a team that is invested in your success, not just a better resume.', 'elevation-career-services' ); ?></p>
-			<a><?php esc_html_e( 'Learn More About Us', 'elevation-career-services' ); ?></a>
+			<?php if ( $heading ) : ?>
+				<h2><?php echo esc_html( $heading ); ?></h2>
+			<?php endif; ?>
+			
+			<p><?php echo nl2br( esc_html( $content ) ); ?></p>
+
+			<?php if ( $link_text && $link ) : ?>
+				<a href="<?php echo esc_url( $link ); ?>">
+					<?php echo esc_html( $link_text ); ?>
+				</a>
+			<?php endif; ?>
 		</div>
 	</div>
 </section>
