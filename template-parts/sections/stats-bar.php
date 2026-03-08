d27b55d95c296ac4760809f9937bfb0ad600cfda
Jamie <jamie7williams@gmail.com>
2026-03-08 15:47:44 +0000

    feat: add Customizer integration to stats bar section

--- template-parts/sections/stats-bar.php
+++ template-parts/sections/stats-bar.php
@@ -1,18 +1,46 @@
-<section class="ecs-section-sm ecs-stats-bar">
-	<div class="ecs-container">
-		<div class="ecs-stat">
-			<p class="ecs-stat-number">50+</p>
-			<p class="ecs-stat-text"><?php esc_html_e( 'Professionals Helped', 'elevation-career-services' ); ?></p>
-		</div>
+<?php
+$stats = array(
+	'stat_1' => array(
+		'num'  => get_theme_mod( 'ecs_stat_1_number' ),
+		'text' => get_theme_mod( 'ecs_stat_1_text' ),
+	),
+	'stat_2' => array(
+		'num'  => get_theme_mod( 'ecs_stat_2_number' ),
+		'text' => get_theme_mod( 'ecs_stat_2_text' ),
+	),
+	'stat_3' => array(
+		'num'  => get_theme_mod( 'ecs_stat_3_number' ),
+		'text' => get_theme_mod( 'ecs_stat_3_text' ),
+	),
+);
 
-		<div class="ecs-stat">
-			<p class="ecs-stat-number">200+</p>
-			<p class="ecs-stat-text"><?php esc_html_e( 'CVs Written', 'elevation-career-services' ); ?></p>
-		</div>
+// Render stats bar only if at least one stat has both a number and text set
+$has_stats = false;
+foreach ( $stats as $stat ) {
+	if ( $stat['num'] && $stat['text'] ) {
+		$has_stats = true;
+		break;
+	}
+}
 
-		<div class="ecs-stat">
-			<p class="ecs-stat-number">98%</p>
-			<p class="ecs-stat-text"><?php esc_html_e( 'Client Satisfaction', 'elevation-career-services' ); ?></p>
+if ( $has_stats ) :
+	?>
+	<section class="ecs-section-sm ecs-stats-bar">
+		<div class="ecs-container">
+			<?php
+			foreach ( $stats as $stat ) :
+				if ( $stat['num'] && $stat['text'] ) :
+					?>
+					<div class="ecs-stat">
+						<p class="ecs-stat-number"><?php echo esc_html( $stat['num'] ); ?></p>
+						<p class="ecs-stat-text"><?php echo esc_html( $stat['text'] ); ?></p>
+					</div>
+					<?php
+				endif;
+			endforeach;
+			?>
 		</div>
-	</div>
-</section>
+	</section>
+	<?php
+endif;
+?>
