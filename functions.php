1e2344b7da595ac84969561b24aca4495ed6b253
Jamie <jamie7williams@gmail.com>
2026-03-10 12:30:11 +0000

    feat: create ACF field for service icons in acf.php

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ require_once $template_dir . '/includes/theme-setup.php';
 require_once $template_dir . '/includes/enqueue.php';
 require_once $template_dir . '/includes/customizer.php';
 require_once $template_dir . '/includes/woocommerce.php';
+require_once $template_dir . '/includes/acf.php';
 
 // Hide Admin Bar
 add_filter( 'show_admin_bar', '__return_false' );
---
+++ includes/acf.php
@@ -0,0 +1,67 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_action(
+	'acf/include_fields',
+	function () {
+		if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+			return;
+		}
+
+		acf_add_local_field_group(
+			array(
+				'key'                   => 'group_69b009ec76815',
+				'title'                 => 'Service Options',
+				'fields'                => array(
+					array(
+						'key'               => 'field_69b009edfd5e4',
+						'label'             => 'Service Icon',
+						'name'              => 'service_icon',
+						'aria-label'        => '',
+						'type'              => 'image',
+						'instructions'      => 'Upload a square icon for this product. SVG or PNG.',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						),
+						'return_format'     => 'array',
+						'library'           => 'all',
+						'min_width'         => '',
+						'min_height'        => '',
+						'min_size'          => '',
+						'max_width'         => '',
+						'max_height'        => '',
+						'max_size'          => '',
+						'mime_types'        => 'svg, png',
+						'allow_in_bindings' => 0,
+						'preview_size'      => 'thumbnail',
+					),
+				),
+				'location'              => array(
+					array(
+						array(
+							'param'    => 'post_type',
+							'operator' => '==',
+							'value'    => 'product',
+						),
+					),
+				),
+				'menu_order'            => 0,
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => true,
+				'description'           => '',
+				'show_in_rest'          => 0,
+				'display_title'         => '',
+			)
+		);
+	}
+);
