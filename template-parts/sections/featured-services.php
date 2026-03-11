0c17a2f85ed6f23d6e1ddbca094a7d352f941522
Jamie <jamie7williams@gmail.com>
2026-03-11 18:53:56 +0000

    feat: add featured services and testimonials template part markup and layout

--- template-parts/sections/featured-services.php
+++ template-parts/sections/featured-services.php
@@ -1,1 +1,12 @@
-<?php echo do_blocks( '<!-- wp:ecs/featured-services /-->' );
+<section class="ecs-section-lg ecs-featured-services">
+	<div class="ecs-container">
+		<div class="ecs-front-page-section-titles">
+			<h2><?php esc_html_e( 'Our Services', 'elevation-career-services' ); ?></h2>
+			<p><?php esc_html_e( 'Everything you need to get hired', 'elevation-career-services' ); ?></p>
+		</div>
+
+		<?php echo do_blocks( '<!-- wp:ecs/featured-services /-->' ); ?>
+
+		<a><?php esc_html_e( 'View All Services', 'elevation-career-services' ); ?></a>
+	</div>
+</section>
--- template-parts/sections/testimonials.php
+++ template-parts/sections/testimonials.php
@@ -1,1 +1,10 @@
-<?php echo do_blocks( '<!-- wp:ecs/testimonials-slider /-->' );
+<section class="ecs-section-lg ecs-testimonials">
+	<div class="ecs-container">
+		<div class="ecs-front-page-section-titles">
+			<h2><?php esc_html_e( 'What Our Clients Say', 'elevation-career-services' ); ?></h2>
+			<p><?php esc_html_e( 'Real results from real people', 'elevation-career-services' ); ?></p>
+		</div>
+
+		<?php echo do_blocks( '<!-- wp:ecs/testimonials-slider /-->' ); ?>
+	</div>
+</section>
