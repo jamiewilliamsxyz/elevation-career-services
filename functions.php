0be7089cf3e341b328fa9e5dba294ba0390cb1b3
Jamie <jamie7williams@gmail.com>
2026-03-11 18:40:33 +0000

    feat: scaffold and register featured services and testimonials slider blocks

--- functions.php
+++ functions.php
@@ -9,5 +9,13 @@ require_once $template_dir . '/includes/woocommerce.php';
 require_once $template_dir . '/includes/acf.php';
 require_once $template_dir . '/includes/post-types.php';
 
+// Register Block Types
+add_action( 'init', 'ecs_register_block_types' );
+
+function ecs_register_block_types() {
+	register_block_type( get_template_directory() . '/blocks/build/featured-services' );
+	register_block_type( get_template_directory() . '/blocks/build/testimonials-slider' );
+}
+
 // Hide Admin Bar
 add_filter( 'show_admin_bar', '__return_false' );
