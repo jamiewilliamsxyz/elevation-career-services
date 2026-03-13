4025f93009e08b584fb6ce0d8ef816e8a62316e4
Jamie <jamie7williams@gmail.com>
2026-03-13 15:15:23 +0000

    style: update content.php link text to 'Read more'

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -4,6 +4,6 @@
 	<span class="ecs-text-sm"><?php the_excerpt(); ?></span>
 
 	<a href="<?php the_permalink(); ?>">
-		<?php esc_html_e( 'Find out more', 'elevation-career-services' ); ?>
+		<?php esc_html_e( 'Read more', 'elevation-career-services' ); ?>
 	</a>
 </article>
