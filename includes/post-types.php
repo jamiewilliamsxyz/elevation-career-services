1287891a83ff7eeca9e8ed04f462ff1cbb7b3b44
Jamie <jamie7williams@gmail.com>
2026-03-13 15:10:09 +0000

    feat: create resource custom post type in post-types.php

--- includes/post-types.php
+++ includes/post-types.php
@@ -55,4 +55,61 @@ function ecs_register_post_types() {
 			'delete_with_user'    => false,
 		)
 	);
+
+	// Resources
+	register_post_type(
+		'ecs_resource',
+		array(
+			'labels'           => array(
+				'name'                     => 'Resources',
+				'singular_name'            => 'Resource',
+				'menu_name'                => 'Resources',
+				'all_items'                => 'All Resources',
+				'edit_item'                => 'Edit Resource',
+				'view_item'                => 'View Resource',
+				'view_items'               => 'View Resources',
+				'add_new_item'             => 'Add New Resource',
+				'add_new'                  => 'Add New Resource',
+				'new_item'                 => 'New Resource',
+				'parent_item_colon'        => 'Parent Resource:',
+				'search_items'             => 'Search Resources',
+				'not_found'                => 'No resources found',
+				'not_found_in_trash'       => 'No resources found in the bin',
+				'archives'                 => 'Resource Archives',
+				'attributes'               => 'Resource Attributes',
+				'insert_into_item'         => 'Insert into resource',
+				'uploaded_to_this_item'    => 'Uploaded to this resource',
+				'filter_items_list'        => 'Filter resources list',
+				'filter_by_date'           => 'Filter resources by date',
+				'items_list_navigation'    => 'Resources list navigation',
+				'items_list'               => 'Resources list',
+				'item_published'           => 'Resource published.',
+				'item_published_privately' => 'Resource published privately.',
+				'item_reverted_to_draft'   => 'Resource reverted to draft.',
+				'item_scheduled'           => 'Resource scheduled.',
+				'item_updated'             => 'Resource updated.',
+				'item_link'                => 'Resource Link',
+				'item_link_description'    => 'A link to a resource.',
+			),
+			'description'      => 'Free guides, templates and advice to help you take the next step in your career.',
+			'public'           => true,
+			'show_in_rest'     => true,
+			'menu_icon'        => 'dashicons-portfolio',
+			'supports'         => array(
+				0 => 'title',
+				1 => 'editor',
+				2 => 'excerpt',
+				3 => 'thumbnail',
+				4 => 'custom-fields',
+			),
+			'has_archive'      => 'resources',
+			'rewrite'          => array(
+				'slug'       => 'resources',
+				'with_front' => false,
+				'feeds'      => false,
+			),
+			'query_var'        => 'resources',
+			'delete_with_user' => false,
+		)
+	);
 }
