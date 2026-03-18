c875f26ed878e23a087d2e0cae0274cd2abce808
Jamie <jamie7williams@gmail.com>
2026-03-18 19:54:10 +0000

    style: add checkout page styles

--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -3,9 +3,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'init', 'ecs_setup_woocommerce' );
+// Remove WooCommerce default styles (aside from checkout page)
+add_filter(
+	'woocommerce_enqueue_styles',
+	function ( $styles ) {
+		if ( is_checkout() ) {
+			return $styles;
+		}
+		return array();
+	}
+);
 
-function ecs_setup_woocommerce() {
-	// Remove WooCommerce default styles
-	add_filter( 'woocommerce_enqueue_styles', '__return_false' );
-}
+// Button text
+add_filter(
+	'woocommerce_order_button_text',
+	function () {
+		return __( 'Buy Now', 'elevation-career-services' );
+	}
+);
+
+add_action(
+	'woocommerce_before_checkout_form',
+	function () {
+		remove_action( 'woocommerce_before_checkout_form', 'woocommerce_output_all_notices' );
+	},
+	1
+);
