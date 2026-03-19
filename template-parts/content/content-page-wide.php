26e09880f949352f70744e3ff8c0adcd46da096d
Jamie <jamie7williams@gmail.com>
2026-03-19 11:25:19 +0000

    refactor: update post class name in content-page-wide.php

--- template-parts/content/content-page-wide.php
+++ template-parts/content/content-page-wide.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'ecs-page' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'ecs-page-wide' ); ?>>
 	<?php
 	get_template_part(
 		'template-parts/components/header-banner',
